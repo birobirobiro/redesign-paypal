f82f824a60f9c9ef0b119b976fcd1d58b025b027
Luis Filipe Alves de Oliveira <macbook-pro-lf-oliveira@MacBook-Pro-de-Luis.local>
2020-09-04 22:50:21 -0300

    Adjust iOS, adjust Bottom navigator

--- src/screens/HomeScreen/styles.js
+++ src/screens/HomeScreen/styles.js
@@ -1,88 +1,90 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 18
+    paddingTop: 18,
   },
   header: {
     height: 64,
-    flexDirection: 'row',
+    flexDirection: "row",
     paddingHorizontal: 24,
-    alignItems: 'center',
-    justifyContent: 'space-between'
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   user: {
-    flexDirection: 'row',
-    alignItems: 'center'
+    flexDirection: "row",
+    alignItems: "center",
   },
   avatar: {
     width: 32,
     height: 32,
     borderRadius: 16,
-    marginLeft: 16
+    marginLeft: 16,
   },
   balance: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     paddingHorizontal: 24,
   },
   balanceTitle: {
     fontSize: 14,
-    color: '#FF9076',
+    color: "#FF9076",
   },
   balanceValue: {
     marginTop: 8,
     fontSize: 28,
-    color: '#092071'
+    color: "#092071",
   },
   balanceCard: {
     marginTop: 16,
-    backgroundColor: '#FF9076',
+    backgroundColor: "#FF9076",
     padding: 24,
-    borderRadius: 8
+    borderRadius: 8,
   },
   balanceCardTop: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   balanceCardBottom: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginTop: 38,
-    alignItems: 'flex-end'
+    alignItems: "flex-end",
   },
   balanceCardInfo: {},
   balanceCardInfoTitle: {
     fontSize: 18,
-    color: '#FFF',
+    color: "#FFF",
   },
   balanceCardInfoTitleFeatured: {
     fontSize: 24,
   },
   balanceCardInfoDescription: {
     fontSize: 12,
-    color: '#FFF',
-    marginTop: 8
+    color: "#FFF",
+    marginTop: 8,
   },
   activity: {
     flex: 1,
-    borderWidth: 1,
-    borderColor: 'red',
+    borderWidth: 3,
+    borderColor: "#fff",
+    borderTopEndRadius: 25,
+    borderTopStartRadius: 25,
   },
   activityInfoContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     paddingHorizontal: 24,
-    paddingTop: 24
+    paddingTop: 24,
   },
   activityTitle: {
-    color: '#1A2F7B',
-    fontSize: 18
+    color: "#1A2F7B",
+    fontSize: 18,
   },
   activityHistory: {
     fontSize: 14,
-    color: '#1A2F7B',
+    color: "#1A2F7B",
   },
   scrollContainer: {
     paddingHorizontal: 24,
@@ -95,23 +97,23 @@ const styles = StyleSheet.create({
   },
   activitySectionTitle: {
     fontSize: 16,
-    color: '#1A2F7B',
-    marginBottom: 7
+    color: "#1A2F7B",
+    marginBottom: 7,
   },
   activitySectionCard: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginTop: 14,
     paddingHorizontal: 16,
     paddingVertical: 8,
     elevation: 3,
     borderRadius: 16,
-    backgroundColor: '#FFF'
+    backgroundColor: "#FFF",
   },
   activitySectionCardIconContainer: {
-    backgroundColor: '#FF9076',
+    backgroundColor: "#FF9076",
     padding: 10,
-    borderRadius: 21
+    borderRadius: 21,
   },
   activitySectionCardInfoContainer: {
     marginLeft: 8,
@@ -119,16 +121,16 @@ const styles = StyleSheet.create({
   },
   activitySectionCardTitle: {
     fontSize: 18,
-    color: '#1A2F7B',
+    color: "#1A2F7B",
   },
   activitySectionCardDescription: {
     fontSize: 14,
-    color: '#A0A0A0',
+    color: "#A0A0A0",
   },
   activitySectionCardValue: {
     fontSize: 18,
-    color: '#5CBC71',
+    color: "#5CBC71",
   },
-})
+});
 
-export default styles
+export default styles;
